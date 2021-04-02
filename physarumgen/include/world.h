a0416bb4f29e3458a530adbcf9a9554c3e953f80
szymon <szymon.kowalewski@gmail.com>
2021-04-02 19:06:42 +0200

    Add initial version of world class

---
+++ physarumgen/include/world.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "agent.h"
+#include <vector>
+
+class c_world
+{
+    public:
+        c_world(unsigned int height, unsigned int width);
+        void spawn_agents(float world_percentage);
+    private:
+        std::vector<c_agent> m_agents;
+        unsigned int m_height;
+        unsigned int m_width;
+}
---
+++ physarumgen/src/world.cpp
@@ -0,0 +1,6 @@
+#include "world.h"
+
+c_world::c_world(unsigned int height, unsigned int width)
+: m_height{height}
+, m_width{width}
+{}
