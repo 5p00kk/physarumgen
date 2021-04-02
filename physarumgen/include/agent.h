a2325698f480921b8f15cbaee82139ccd621d37a
szymon <szymon.kowalewski@gmail.com>
2021-04-02 18:59:35 +0200

    Add initial version of the agent

---
+++ physarumgen/include/agent.h
@@ -0,0 +1,20 @@
+#pragma once
+
+struct s_position
+{
+    unsigned int x;
+    unsigned int y;
+};
+
+class c_agent
+{
+    public:
+        c_agent();
+        c_agent(unsigned int pos_x, unsigned int pos_y, float angle);
+        void print_info() const;
+        s_position query_move() const;
+        void move();
+    private:
+        s_position m_position;
+        float m_angle;
+};
---
+++ physarumgen/src/agent.cpp
@@ -0,0 +1,23 @@
+#include "agent.h"
+#include <iostream>
+
+c_agent::c_agent()
+{
+    /* TODO set random values */
+    std::cout << "Created c_agent using random values" << std::endl;
+}
+
+c_agent::c_agent(unsigned int pos_x, unsigned int pos_y, float angle)
+    : m_position{pos_x, pos_y}
+    , m_angle{angle}
+{
+    std::cout << "Created c_agent using user values" << std::endl;
+}
+
+void c_agent::print_info() const
+{
+    std::cout << std::endl << "Printing c_agent info" << std::endl;
+    std::cout << "x: " << m_position.x << std::endl;
+    std::cout << "y: " << m_position.y << std::endl;
+    std::cout << "a: " << m_angle << std::endl << std::endl;
+}
