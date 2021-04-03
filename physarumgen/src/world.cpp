8552a175f4c483f402c8a7ede6afac4ea6dc8b96
szymon <szymon.kowalewski@gmail.com>
2021-04-03 13:42:40 +0200

    get angle from world sampler

--- physarumgen/src/world.cpp
+++ physarumgen/src/world.cpp
@@ -27,7 +27,7 @@ bool c_world::spawn_agents(float world_percentage)
     for(int i=0; i<num_agents; i++)
     {
         s_position tmp_pos = m_world_sampler.get_next_position();
-        m_agents.emplace_back(tmp_pos.x, tmp_pos.y, 0.0);
+        m_agents.emplace_back(tmp_pos.x, tmp_pos.y, m_world_sampler.get_angle());
     }
 
     std::cout << "Spawned " << m_agents.size() << " agents" << std::endl;
