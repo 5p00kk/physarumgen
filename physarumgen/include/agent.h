20cbcf493fa213289258d2621313b72c6fd534ca
szymon <szymon.kowalewski@gmail.com>
2021-04-03 13:31:13 +0200

    simplify agent for now

--- physarumgen/include/agent.h
+++ physarumgen/include/agent.h
@@ -7,8 +7,6 @@ class c_agent
     public:
         c_agent(unsigned int pos_x, unsigned int pos_y, float angle);
         void print_info() const;
-        s_position query_move() const;
-        void move();
         s_position m_position;
         float m_angle;
     private:
