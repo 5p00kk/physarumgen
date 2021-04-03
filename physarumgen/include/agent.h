03fb68b82ed6137bc9db1f8c2b8b6188aa419703
szymon <szymon.kowalewski@gmail.com>
2021-04-03 14:04:41 +0200

    add extra agent parameters

--- physarumgen/include/agent.h
+++ physarumgen/include/agent.h
@@ -9,6 +9,8 @@ class c_agent
         void print_info() const;
         s_position m_position;
         float m_alpha;
-        float m_sensor_angle;
+        float m_sensor_angle = 45.0f;
+        float m_rotation_angle = 45.0f;
+        unsigned int m_deposition_value = 5;
     private:
 };
