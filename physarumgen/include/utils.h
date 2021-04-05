ed8272e4316e2cd3c931f1b6f7049ba5d104660b
szymon <szymon.kowalewski@gmail.com>
2021-04-05 13:31:28 +0200

    Document utils.h

--- physarumgen/include/utils.h
+++ physarumgen/include/utils.h
@@ -1,10 +1,18 @@
+/*!
+ \file
+*/
+
 #pragma once
 
 #include <cmath>
 
 #define PI 3.14159265
 
-/* TODO Maybe use template here */
+/* TODO Maybe use template for these vectors */
+
+/*!
+    \brief 2D unsigned int vector.
+*/
 struct s_ui_vec2
 {
     unsigned int x;
@@ -13,6 +21,9 @@ struct s_ui_vec2
 bool operator==(const s_ui_vec2& lhs, const s_ui_vec2& rhs);
 bool operator!=(const s_ui_vec2& lhs, const s_ui_vec2& rhs);
 
+/*!
+    \brief 2D float vector.
+*/
 struct s_f_vec2
 {
     float x;
@@ -21,5 +32,13 @@ struct s_f_vec2
 bool operator==(const s_f_vec2& lhs, const s_f_vec2& rhs);
 bool operator!=(const s_f_vec2& lhs, const s_f_vec2& rhs);
 
+/*!
+    \brief Sample random boolean.
+    \return Random boolean.
+*/
 bool random_bool();
+/*!
+    \brief Sample random angle.
+    \return Float in [0-359] range with increment of 1.
+*/
 float random_angle();
