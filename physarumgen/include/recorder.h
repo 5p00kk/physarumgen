cfe78b7f5689a949a391b440354a546305f628e6
szymon <szymon.kowalewski@gmail.com>
2021-04-05 17:12:48 +0200

    Implement recorder

--- physarumgen/include/recorder.h
+++ physarumgen/include/recorder.h
@@ -1,14 +1,17 @@
 #pragma once
 
 #include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
 #include <string>
 
 class c_recorder
 {
     public:
         c_recorder(unsigned int width, unsigned int height, const std::string &path);
-        void video_add_frame(const cv::Mat &frame) const;
-        void save_image(const cv::Mat &frame) const;
+        void video_add_frame(const cv::Mat &frame);
+        void save_image(const cv::Mat &frame);
     private:
+        std::string m_path;
         unsigned int m_frame_number = 0;
+        cv::VideoWriter m_video_writer;
 };
--- physarumgen/src/recorder.cpp
+++ physarumgen/src/recorder.cpp
@@ -1,3 +1,21 @@
-#include "recorder.h"
-#include <opencv2/videoio.hpp>
+/* TODO fix all these paths etc */
 
+#include "recorder.h"
+#include <opencv2/imgcodecs.hpp>
+
+c_recorder::c_recorder(unsigned int width, unsigned int height, const std::string &path)
+    : m_path{path}
+    , m_video_writer{(path+"physarum.mp4"), cv::VideoWriter::fourcc('A', 'V', 'C', '1'), 30, cv::Size(height, width), false}
+{}
+
+void c_recorder::video_add_frame(const cv::Mat &frame)
+{
+    m_video_writer.write(frame);
+}
+
+void c_recorder::save_image(const cv::Mat &frame)
+{
+    std::string image_path = m_path + "physarum_" + std::to_string(m_frame_number) + ".jpg";
+    cv::imwrite(image_path, frame);
+    m_frame_number++;
+}
