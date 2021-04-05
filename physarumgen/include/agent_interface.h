f8cac5cb2fbe7bf1a1a9f14bfd6f02fd4dc13760
szymon <szymon.kowalewski@gmail.com>
2021-04-05 12:05:01 +0200

    Add agent's documentation

--- physarumgen/include/agent_interface.h
+++ physarumgen/include/agent_interface.h
@@ -3,20 +3,56 @@
 #include "world.h"
 #include "utils.h"
 
+/*!
+    \brief Agent's 2D pose: position (x,y) and orientation (alpha).
+*/
 struct s_agent_pose
 {
-    s_f_vec2 position;
-    float alpha;
+    s_f_vec2 position; /*!< Agent's 2D position */
+    float alpha;       /*!< Agent's orientation */
 };
 
+/*! 
+    \brief Abstract class for agents.
+
+    The interface requires implemnentations to provide methods for agent's
+    movement and sensing.
+*/
 class i_agent
 {
     public:
+        /*!
+            \brief Constructor
+            \param pos_x Initial x position of the agent.
+            \param pos_y Initial y position of the agent.
+            \param alpha Initial direction of the agent.
+            \param world Pointer to the c_world object in which the agent is spawned.
+        */
         i_agent(float pos_x, float pos_y, float alpha, c_world *world);
+
+        /*!
+        \brief Print agent information
+        */
         virtual void print_info() const = 0;
+
+        /*!
+        \brief Move the agent to the new position.
+        */
         virtual void movement_step() = 0;
+
+        /*!
+        \brief Sense the world.
+        */
         virtual void sensing_step() = 0;
     protected:
+
+        /*!
+            \brief Current pose (position, orientation) of the agent.
+        */
         s_agent_pose m_pose;
+
+        /*!
+            \brief Pointer to the world in which the agent lives.
+        */
         c_world *m_world;
 };
--- physarumgen/include/agent_physarum.h
+++ physarumgen/include/agent_physarum.h
@@ -2,15 +2,23 @@
 
 #include "agent_interface.h"
 
+/*! 
+    \brief Parameters defining Physarum agent.
+*/
 struct s_physarum_params
 {
-    float sensor_angle = 45.0f;
-    float sensor_offset = 9.0f;
-    float rotation_angle = 45.0f;
-    float step_size = 1.0f;
-    unsigned int deposition_value = 5;
+    float sensor_angle = 45.0f;       /*!< Angle of the left and right sensors from the center */
+    float sensor_offset = 9.0f;       /*!< How far from the agent the sensor is located */
+    float rotation_angle = 45.0f;     /*!< How much does the agent rotate on orientation change */
+    float step_size = 1.0f;           /*!< Agent's forward step size */
+    unsigned int deposition_value = 5;    /*!< How much trail is being depoited at once */
 };
 
+/*! 
+    \brief Physarum agent implementation.
+
+    This class implementes an agent based on Jone's paper.
+*/
 class c_physarum: public i_agent
 {
     public:
@@ -19,6 +27,17 @@ class c_physarum: public i_agent
         void movement_step() override;
         void sensing_step() override;
     private:
+        /*!
+            \brief Based on starting point, angle and distance, calculate the end point.
+            \param start_pt Starting 2D point.
+            \param angle Direction of the movement.
+            \param distance Distance covered.
+            \returns 2D end point
+        */
         s_f_vec2 calc_destination(const s_f_vec2 &start_pt, float angle, float distance);
+
+        /*!
+            \brief Physarum agent parameters.
+        */
         s_physarum_params m_params;
 };
