d1d5248e588cb9bb680ab3979025781b0206418c
szymon <szymon.kowalewski@gmail.com>
2021-04-06 17:53:13 +0200

    add new_recording function

--- physarumgen/src/recorder.cpp
+++ physarumgen/src/recorder.cpp
@@ -11,6 +11,12 @@ c_recorder::c_recorder(unsigned int width, unsigned int height, const std::strin
     , m_video_writer{(path+"physarum.mp4"), cv::VideoWriter::fourcc('A', 'V', 'C', '1'), 30, cv::Size(height, width), false}
 {}
 
+void c_recorder::new_recording(unsigned int width, unsigned int height, const std::string &path)
+{
+    m_path = path;
+    m_video_writer.open((path+"physarum.mp4"), cv::VideoWriter::fourcc('A', 'V', 'C', '1'), 30, cv::Size(height, width), false);
+}
+
 void c_recorder::video_add_frame(const cv::Mat &frame)
 {
     m_video_writer.write(frame);
