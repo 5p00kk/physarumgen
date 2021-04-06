22b37649089326ea02f45c41bc9fe39a3bee500a
szymon <szymon.kowalewski@gmail.com>
2021-04-06 18:20:58 +0200

    update demo main

--- demo/main.cpp
+++ demo/main.cpp
@@ -3,10 +3,25 @@
 
 int main()
 {
-    c_simulation simulation(600, 600);
-    simulation.spawn_agents(0.05);
-    
-    for(int i=0;i<10000;i++)
+    /* Simulation params */
+    const int world_width = 300;
+    const int world_height = 600;
+    float population = 0.1;
+    float decay_mult = 0.1f;
+    float decay_sub = 0.5f;
+    unsigned int diff_size = 3;
+
+    /* Prepare simulation */
+    c_simulation simulation(world_width, world_height);
+    simulation.spawn_agents(population);
+    simulation.set_world_decay(decay_mult, decay_sub);
+    simulation.set_world_diffusion(diff_size);
+
+    /* Start recording */
+    simulation.start_recording("rec/");
+
+    /* Run simulation */   
+    for(int i=0;i<300;i++)
     {
         simulation.tick();
     }
