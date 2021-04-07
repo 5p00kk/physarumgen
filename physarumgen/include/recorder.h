aa31315f8c854aa9e7c836dcc6c5a7b72d0ce7c9
szymon <szymon.kowalewski@gmail.com>
2021-04-07 18:35:26 +0200

    Document recorder class

--- physarumgen/include/recorder.h
+++ physarumgen/include/recorder.h
@@ -7,12 +7,44 @@
 class c_recorder
 {
     public:
+        /*!
+            \brief Constructor. No recording started
+        */
         c_recorder();
+        /*!
+            \brief Constructor. Starts recording automatically
+            \param width Width of the recording
+            \param height Height of the recording
+            \param path Folder where recording should be saved
+            \note For now path has to end with '/', no checks if path exsits.
+        */
         c_recorder(unsigned int width, unsigned int height, const std::string &path);
+        /*!
+            \brief Start new recording.
+            \param width Width of the recording
+            \param height Height of the recording
+            \param path Folder where recording should be saved
+            \note For now path has to end with '/', no checks if path exsits.
+        */
         bool new_recording(unsigned int width, unsigned int height, const std::string &path);
+        /*!
+            \brief Stop current recording.
+        */
         void stop_recording();
+        /*!
+            \brief Add frame to the video recording.
+            \param frame Video frame
+            \note <frame> size has to match the vidoe size selected when starting a recording.
+        */
         void video_add_frame(const cv::Mat &frame);
+        /*!
+            \brief Save image snapshot into the recording folder.
+            \param frame Image frame
+        */
         void save_image(const cv::Mat &frame);
+        /*!
+            \brief Is recording active.
+        */
         bool m_recording = false;
     private:
         cv::VideoWriter m_video_writer;
